e5eb8341bfba4f191c82b7eec0ee18bddb02687d
Radi Totev <radi.totev@hostmaker.co>
2018-11-11 18:15:49 +0000

    Create md previewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MarkdownPreview } from 'react-marked-markdown';
 
 class App extends Component {
+  state = {
+    text: ''
+  }
+
+  handleClick = e => {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  style = {
+    width: 300,
+    height: 300,
+    border: 'solid black 1px',
+    align: 'center'
+  }
+
+  cardStyle = {
+    maxWidth: '18rem'
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="card bg-light mb-3">
+          <div className="card-header">Editor</div>
+          <textarea id="editor" rows="1" cols="150" placeholder="Enter markdown" onChange={ this.handleClick }></textarea>
+        </div>
+
+        <div className="card text-white bg-dark mb-3">
+          <div className="card-header">Preview</div>
+          <div class="card-body">
+            <div id="preview">
+              <MarkdownPreview value={ this.state.text } />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
